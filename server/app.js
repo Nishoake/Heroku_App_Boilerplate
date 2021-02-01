9c48494df7ff28b9c954d6d4e607351409c355c0
Nishoake <nsribava@uwaterloo.ca>
2021-02-01 13:19:34 -0500

    Intial integration of Express and Apollo Server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const graphqlExpress = require('apollo-server-express')
+
+const myGraphQLSchema = // ... define or import your schema here!
 
 require('dotenv').config()
 
@@ -7,11 +10,12 @@ app = express()
 
 // Middleware
 app.use(bodyParser.json())
+app.use('/graphql', bodyParser.json(), graphqlExpress({ schema: myGraphQLSchema }))
 
 // Intialize port
 const port = process.env.PORT || 3006
 app.listen(port, () => {
-  console.log(`Gym Share app is currently listening at ${port}`)
+  console.log(`Gym Share Prototype is currently listening at ${port}`)
 })
 
 // Routes
